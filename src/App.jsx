72bf4cc5bd63ec8e549e19aa9046b8297322f394
Asem Hamdi <49765634+AsemDevs@users.noreply.github.com>
2023-11-18 13:29:58 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 function App() {
+  const name = "All The Devs"
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="../images/aneirin.png" />
+          <h1>Meet <span style={{color:"steelblue"}}>{name}</span></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <button className="outline" onClick={()=>alert()}>Click Me</button>
+        </hgroup>
+      </article>
+
+    </div>
+
   )
 }
 export default App
