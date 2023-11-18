8b2a54b764443d306c28228e64c2b9aa02d84fc2
Asem Hamdi <49765634+AsemDevs@users.noreply.github.com>
2023-11-18 14:13:29 +0000

    practicing notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React, { Component } from 'react';
+
+const Hello = (props) => {
+  return (
+    <h1>Meet The <i>{props.name}</i></h1>
+  )
+}
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Meet The { this.props.name }</h1>;
+  }
+}
+
 function App() {
   const name = 'StarGazers'
   return (
     <div className="container">
       <article>
         <hgroup>
+
+          <Welcome name="All The Devs"/>
+          <Hello name="All The coders"/>
+          <Hello name="All The Aliens"/>
+
           <img src="images/group.svg" alt="StarGazers Group" />
           <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b><br />
