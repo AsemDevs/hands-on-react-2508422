6c3924149432148e6991f1bc3eb10213c389f060
Asem Abdo <asemhamdi97@gmail.com>
2023-11-18 23:45:46 +0300

    If condition inside object

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Welcome from './components/Welcome';
+import Support from './components/Support';
 
 function App() {
   const name = 'StarGazers'
@@ -8,10 +9,10 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="Stars" />
+          <Welcome name="All The Devs" />
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+        <Support />
         </hgroup>
       </article>
     </div>
---
+++ src/components/Support.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+export default () => {
+  const [count, setCount] = useState(0);
+
+  return (
+      <button className="outline" onClick={() => setCount(count + 1)}>
+        {
+          (()=>{
+            if (count === 0) {
+            return "Click to Support";
+            } else {
+              return `Supported ${count} times!`
+            }
+          })()
+        }
+      </button>
+  );
+}
